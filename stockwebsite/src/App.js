489c76a031d9365139656d054f80b29cd75e5e3e
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2021-02-24 15:19:43 -0500

    scaffold API

--- stockwebsite/src/App.js
+++ stockwebsite/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  fetchData = () =>{
+    
+    axios.get("https://www.alphavantage.co/query", {
+      params:{ 
+         function: "TIME_SERIES_DAILY_ADJUSTED",
+         symbol: "IBM", //this can be changed depending on which stock we want
+         apikey: "1WKONX2HMTRYF2JO",
+   }})
+
+    .then(res => {
+      
+      this.setState({data: res.data["Time Series (Daily)"]})
+      this.setState({data: this.state.data["2020-09-30"]})
+      //date can be changed later 
+     
+     
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  renderData = () => {
+    if (this.state.data){
+      console.log("inside if")
+      console.log(this.state.data)
+     
+      return(
+        <div>
+          Open: {this.state.data["1. open"]}
+        </div>
+      )
+    } else{
+      console.log("no data")
+      return null
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick = {this.fetchData}>click to fetch data</button>
+        {this.renderData()}
+      </div>
+    );
+  }
+
 }
 
 export default App;
