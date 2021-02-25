ec05a3cc77db1a3ef0b14c955935d7fc925a1148
emschu100 <emily.a.schuster.24@dartmouth.edu>
2021-02-24 19:41:02 -0500

    dropdown bar added

--- stockwebsite/src/App.js
+++ stockwebsite/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import './App.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -9,8 +13,19 @@ function App() {
       <p className="App-body">Username:</p>
       
       <p className="App-body">Add a Stock:</p>
+
+      
+      
+  
+      <DropdownButton id="dropdown-item-button" title="Add a Stock">
+      <Dropdown.Item as="button">AAPL</Dropdown.Item>
+      <Dropdown.Item as="button">NFLX</Dropdown.Item>
+      <Dropdown.Item as="button">PNRA</Dropdown.Item>
+</DropdownButton>
+
     </div>
-  );
-}
+   
+    );}
+    
 
 export default App;
